4a8a04c4dea4f9da251fdbbfed07b1927e154df4
Louis Xu <louis.s.xu@gmail.com>
2022-12-01 23:21:35 +1030

    wip(main): Testing with chopstick pairs

--- philo_bonus/src/setup.c
+++ philo_bonus/src/setup.c
@@ -26,7 +26,7 @@ void	setup_semaphores(t_main_data *m)
 {
 	clean_up_zombie_semaphores();
 	m->sem.mutex = sem_open(SEM_MUTEX, O_CREAT | O_EXCL, 0600, 1);
-	m->sem.chopsticks = sem_open(SEM_CHOPSTICKS, O_CREAT | O_EXCL, 0600, m->input.number_of_philosophers);
+	m->sem.chopsticks = sem_open(SEM_CHOPSTICKS, O_CREAT | O_EXCL, 0600, m->input.number_of_philosophers / 2);
 	m->sem.full = sem_open(SEM_FULL, O_CREAT | O_EXCL, 0600, 0);
 	m->sem.stop = sem_open(SEM_STOP, O_CREAT | O_EXCL, 0600, 0);
 }
