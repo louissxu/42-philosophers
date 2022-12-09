fca435d2420348d3b3e45f5bbd533ba2b76c6935
Louis Xu <louis.s.xu@gmail.com>
2022-12-09 16:35:36 +1030

    fix(main): Hack fix. Don't know why reopening the semaphore is necessary to close it

--- philo_bonus/src/clean_up.c
+++ philo_bonus/src/clean_up.c
@@ -2,10 +2,13 @@
 
 void	close_semaphores(t_main_data *m)
 {
+	printf("closing semaphores\n");
 	sem_close(m->sem.mutex);
 	sem_unlink(SEM_MUTEX);
 	sem_close(m->sem.chopsticks);
 	sem_unlink(SEM_CHOPSTICKS);
+	m->sem.full = sem_open(SEM_FULL, 0);
+	sem_close(m->sem.full);
 	sem_close(m->sem.full);
 	sem_unlink(SEM_FULL);
 	sem_close(m->sem.stop);
